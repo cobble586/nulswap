718baa562684e5178d3dc3eb5af1f6256fba8ee0
Pedro Ferreira <81267194+Pedro-Ferreira-89@users.noreply.github.com>
2024-11-02 02:54:05 +0000

    More functions

--- NulswapRouter/src/main/java/NulswapRouter.java
+++ NulswapRouter/src/main/java/NulswapRouter.java
@@ -755,21 +755,51 @@ public class NulswapRouter extends Ownable implements Contract{
         }
     }
 
-    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
-            uint amountIn,
-            uint amountOutMin,
-            address[] calldata path,
-            address to,
-            uint deadline
-    ) external virtual override ensure(deadline) {
-        TransferHelper.safeTransferFrom(
-                path[0], msg.sender, UniswapV2Library.pairFor(factory, path[0], path[1]), amountIn
+    public void swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            BigInteger amountIn,
+            BigInteger amountOutMin,
+            String[] path,
+            Address to,
+            BigInteger deadline
+    )  {
+        ensure(deadline);
+        blacklist();
+
+        safeTransferFrom(
+                new Address(path[0]), Msg.sender(), safeGetPair(new Address(path[0]), new Address(path[1])), amountIn
         );
-        uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
+        BigInteger balanceBefore = safeBalanceOf(new Address(path[path.length - 1]), to);
         _swapSupportingFeeOnTransferTokens(path, to);
         require(
-                IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
-                'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT'
+                (safeBalanceOf(new Address(path[path.length - 1]), to).subtract(balanceBefore)).compareTo(amountOutMin) >= 0,
+                "UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT"
+        );
+    }
+
+    @Payable
+    public void  swapExactETHForTokensSupportingFeeOnTransferTokens(
+            BigInteger amountOutMin,
+            String[] path,
+            Address to,
+            BigInteger deadline
+    )
+
+
+    {
+        ensure(deadline);
+        blacklist();
+
+        require(new Address(path[0]).equals(WNULS), "UniswapV2Router: INVALID_PATH");
+
+        BigInteger amountIn = Msg.value();
+        depositNuls(amountIn);
+
+        safeTransferETH(safeGetPair(new Address(path[0]), new Address(path[1])), amountIn);
+        BigInteger balanceBefore = safeBalanceOf(new Address(path[path.length - 1]), to);//IERC20(path[path.length - 1]).balanceOf(to);
+        _swapSupportingFeeOnTransferTokens(path, to);
+        require(
+                safeBalanceOf(new Address(path[path.length - 1]), to).subtract(balanceBefore).compareTo(amountOutMin) >= 0,
+                "UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT"
         );
     }
 
