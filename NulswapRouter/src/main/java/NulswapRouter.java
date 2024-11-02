935cf8e0317cdee25d3a0eea009e30c31eb925bc
Pedro Ferreira <81267194+Pedro-Ferreira-89@users.noreply.github.com>
2024-11-02 03:37:18 +0000

    .

--- NulswapRouter/src/main/java/NulswapRouter.java
+++ NulswapRouter/src/main/java/NulswapRouter.java
@@ -803,6 +803,29 @@ public class NulswapRouter extends Ownable implements Contract{
         );
     }
 
+    public void swapExactTokensForETHSupportingFeeOnTransferTokens(
+           BigInteger amountIn,
+            BigInteger amountOutMin,
+            String[]  path,
+            Address to,
+            BigInteger deadline
+    )
+    {
+        ensure(deadline)
+        blacklist()
+
+        require(path[path.length - 1] == WETH, "UniswapV2Router: INVALID_PATH");
+        safeTransferFrom(
+                path[0], msg.sender, UniswapV2Library.pairFor(factory, path[0], path[1]), amountIn
+        );
+        _swapSupportingFeeOnTransferTokens(path, address(this));
+        uint amountOut = IERC20(WETH).balanceOf(address(this));
+        require(amountOut >= amountOutMin, "UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT");
+
+        withdrawNuls(amountOut);
+        safeTransferETH(to, amountOut);
+    }
+
     /**
      *
      *
