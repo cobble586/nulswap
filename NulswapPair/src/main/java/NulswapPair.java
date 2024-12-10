d6170071660c240d9ca87e0fb024f10de12978ad
Pedro Ferreira <81267194+Pedro-Ferreira-89@users.noreply.github.com>
2024-12-10 23:13:38 +0000

    Nulswap pair Update

--- NulswapPair/src/main/java/NulswapPair.java
+++ NulswapPair/src/main/java/NulswapPair.java
@@ -150,7 +150,7 @@ public class NulswapPair implements Contract{
         reserve0            = balance0;
         reserve1            = balance1;
         blockTimestampLast  = blockTimestamp;
-        //emit Sync(reserve0, reserve1);
+        emit(new SyncEvent(reserve0, reserve1));
     }
 
     // if fee is on, mint liquidity equivalent to 1/6th of the growth in sqrt(k)
@@ -282,7 +282,7 @@ public class NulswapPair implements Contract{
 
         _update(balance0, balance1, reserve0, reserve1);
         if (feeOn) kLast = reserve0.multiply(reserve1); // reserve0 and reserve1 are up-to-date
-        //emit Burn(msg.sender, amount0, amount1, to);
+        emit(new BurnEvent(Msg.sender(), amount0, amount1, to));
 
         unlock();
 
@@ -604,6 +604,129 @@ public class NulswapPair implements Contract{
             if (o == null || getClass() != o.getClass()) return false;
 
             MintEvent that = (MintEvent) o;
+
+            if (sender != null ? !sender.equals(that.sender) : that.sender != null) return false;
+            if (amount0 != null ? !amount0.equals(that.amount0) : that.amount0 != null) return false;
+            return amount1 != null ? amount1.equals(that.amount1) : that.amount1 == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = sender != null ? sender.hashCode() : 0;
+            result = 31 * result + (amount0 != null ? amount0.hashCode() : 0);
+            result = 31 * result + (amount1 != null ? amount1.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Mint{" +
+                    "spender=" + sender +
+                    ", amount0=" + amount0 +
+                    ", amount1=" + amount1 +
+                    '}';
+        }
+
+    }
+
+    class BurnEvent implements Event {
+
+        private Address sender;
+
+        private BigInteger amount0;
+        private BigInteger amount1;
+
+        private Address to;
+
+        public BurnEvent(@Required Address sender, @Required BigInteger amount0, @Required BigInteger amount1, @Required Address to) {
+            this.sender  = sender;
+            this.amount0 = amount0;
+            this.amount1 = amount1;
+            this.to      = to;
+        }
+
+        public Address getSender() {
+            return sender;
+        }
+
+        public void setSender(Address sender) {
+            this.sender = sender;
+        }
+
+        public BigInteger getAmount0() {
+            return amount0;
+        }
+
+        public void setAmount0(BigInteger amount0) {
+            this.amount0 = amount0;
+        }
+
+        public BigInteger getAmount1() {
+            return amount1;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            BurnEvent that = (BurnEvent) o;
+
+            if (sender != null ? !sender.equals(that.sender) : that.sender != null) return false;
+            if (amount0 != null ? !amount0.equals(that.amount0) : that.amount0 != null) return false;
+            return amount1 != null ? amount1.equals(that.amount1) : that.amount1 == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = sender != null ? sender.hashCode() : 0;
+            result = 31 * result + (amount0 != null ? amount0.hashCode() : 0);
+            result = 31 * result + (amount1 != null ? amount1.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Mint{" +
+                    "spender=" + sender +
+                    ", amount0=" + amount0 +
+                    ", amount1=" + amount1 +
+                    '}';
+        }
+
+    }
+
+    class SyncEvent implements Event {
+
+        private Address sender;
+
+        private BigInteger amount0;
+        private BigInteger amount1;
+
+
+        public SyncEvent(@Required BigInteger amount0, @Required BigInteger amount1) {
+            this.amount0 = amount0;
+            this.amount1 = amount1;
+        }
+
+        public BigInteger getAmount0() {
+            return amount0;
+        }
+
+        public void setAmount0(BigInteger amount0) {
+            this.amount0 = amount0;
+        }
+
+        public BigInteger getAmount1() {
+            return amount1;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            SyncEvent that = (SyncEvent) o;
 
             if (sender != null ? !sender.equals(that.sender) : that.sender != null) return false;
             if (amount0 != null ? !amount0.equals(that.amount0) : that.amount0 != null) return false;
